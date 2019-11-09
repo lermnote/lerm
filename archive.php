600df28967ab922b8118082a4bd7e5827cd021ea
hanlong@live.cn <hanlong@live.cn>
2019-11-09 15:55:37 +0800

    taxomony object fix

--- archive.php
+++ archive.php
@@ -8,9 +8,9 @@
  */
 
 get_header();
-$term = get_queried_object();
-if ( $term ) {
-	$meta = get_term_meta( $term->term_id, 'lerm_taxonomy_options', true );
+$lerm_term = get_queried_object();
+if ( $lerm_term ) {
+	$meta = get_term_meta( $lerm_term->term_id, 'lerm_taxonomy_options', true );
 }
 	$bg_color       = isset( $meta['archive_color']['bg_color'] ) ? $meta['archive_color']['bg_color'] : '#fff';
 	$font_color     = isset( $meta['archive_color']['font_color'] ) ? $meta['archive_color']['font_color'] : '#5d6777';
@@ -25,7 +25,7 @@ if ( $term ) {
 		<div class="<?php echo esc_attr( $class ); ?>  px-0" >
 			<div id="main" class="site-main ajax-posts">
 				<?php if ( have_posts() ) : ?>
-						<header class="archive-header mb-2 p-3" style="<?php echo $archive_header; ?>">
+						<header class="archive-header mb-2 p-3" style="<?php echo esc_attr( $archive_header ); ?>">
 						<?php
 						the_archive_title( '<h1 class="page-title">', '</h1>' );
 						the_archive_description( '<small class="taxonomy-description">', '</small>' );
