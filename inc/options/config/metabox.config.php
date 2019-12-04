ebebd2becad54b76cbb3de64754260850f7d17b2
hanlong@live.cn <hanlong@live.cn>
2019-12-04 22:53:12 +0800

    remove unuse code

--- inc/options/config/metabox.config.php
+++ inc/options/config/metabox.config.php
@@ -8,7 +8,7 @@
 // ===============================================================================================
 // If using image radio buttons, define a directory path
 global $lerm;
-//global layout
+// global layout
 $global_layout = $lerm['global_layout'];
 $imagepath     = LERM_URI . 'assets/img/';
 $layout        = array(
@@ -56,99 +56,4 @@ if ( class_exists( 'CSF' ) ) {
 	);
 
 	$prefix_post_opts = '_lerm_post_options';
-
-	//
-	// Create a metabox
-	//
-	CSF::createMetabox(
-		$prefix_post_opts,
-		array(
-			'title'        => 'Custom Post Options',
-			'post_type'    => 'post',
-			'show_restore' => true,
-		)
-	);
-
-	//
-	// Create a section
-	//
-	CSF::createSection(
-		$prefix_post_opts,
-		array(
-			'fields' => array(
-				array(
-					'title'   => __( 'Original', 'lerm' ),
-					'id'      => 'original_switcher',
-					'type'    => 'switcher',
-					'default' => true,
-				),
-
-				//
-				// A text field
-				//
-				array(
-					'id'    => 'opt-text',
-					'type'  => 'text',
-					'title' => 'Text',
-				),
-
-				array(
-					'id'    => 'opt-textarea',
-					'type'  => 'textarea',
-					'title' => 'Textarea',
-					'help'  => 'The help text of the field.',
-				),
-
-				array(
-					'id'    => 'opt-upload',
-					'type'  => 'upload',
-					'title' => 'Upload',
-				),
-
-				array(
-					'id'    => 'opt-switcher',
-					'type'  => 'switcher',
-					'title' => 'Switcher',
-					'label' => 'The label text of the switcher.',
-				),
-
-				array(
-					'id'    => 'opt-color',
-					'type'  => 'color',
-					'title' => 'Color',
-				),
-
-				array(
-					'id'    => 'opt-checkbox',
-					'type'  => 'checkbox',
-					'title' => 'Checkbox',
-					'label' => 'The label text of the checkbox.',
-				),
-
-				array(
-					'id'      => 'opt-radio',
-					'type'    => 'radio',
-					'title'   => 'Radio',
-					'options' => array(
-						'yes' => 'Yes, Please.',
-						'no'  => 'No, Thank you.',
-					),
-					'default' => 'yes',
-				),
-
-				array(
-					'id'          => 'opt-select',
-					'type'        => 'select',
-					'title'       => 'Select',
-					'placeholder' => 'Select an option',
-					'options'     => array(
-						'opt-1' => 'Option 1',
-						'opt-2' => 'Option 2',
-						'opt-3' => 'Option 3',
-					),
-				),
-
-			),
-		)
-	);
 }
