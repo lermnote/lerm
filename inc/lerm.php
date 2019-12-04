c972a367583c6d8178d5359fc7bb4adb57864661
hanlong@live.cn <hanlong@live.cn>
2019-12-04 22:52:16 +0800

    file name change

--- inc/lerm.php
+++ inc/lerm.php
@@ -1,13 +1,14 @@
 <?php
 
-//load class files
-
+// load class files
 require LERM_DIR . 'inc/admin/class-breadcrumb-trail.php';
 require LERM_DIR . 'inc/admin/class-media-grabber.php';
-require LERM_DIR . 'inc/admin/class-nav-menu.php';
+require LERM_DIR . 'inc/admin/class-lerm-walker-nav-menu.php';
 require LERM_DIR . 'inc/admin/class-lerm-svg-icons.php';
+require LERM_DIR . 'inc/admin/class-lerm-walker-comment.php';
 
-//load function files
+// load function files
+require LERM_DIR . 'inc/admin/function-like.php';
 require LERM_DIR . 'inc/admin/function-slide.php';
 require LERM_DIR . 'inc/admin/function-mail.php';
 require LERM_DIR . 'inc/admin/function-login.php';
@@ -18,9 +19,6 @@ require LERM_DIR . 'inc/admin/function-load-more.php';
 require LERM_DIR . 'inc/admin/icon-functions.php';
 require LERM_DIR . 'inc/admin/functions.opengraph.php';
 require LERM_DIR . 'inc/admin/related.php';
-// Load shortcode files
-// require LERM_DIR . 'inc/admin/shortcode.php';
 
 // Load the template files
-require LERM_DIR . 'inc/admin/template-comment.php';
 require LERM_DIR . 'inc/admin/zh_to_py.php';
