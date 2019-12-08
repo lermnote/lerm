ec9aa3f606ac7eb7cf5d36c23df5cb632a09e9c5
hanlong@live.cn <hanlong@live.cn>
2019-12-08 14:02:30 +0800

    related posts

--- single.php
+++ single.php
@@ -23,13 +23,13 @@ get_header();
 						get_template_part( 'template/content/content', 'single' );
 						?>
 							<?php lerm_entry_tag(); ?>
-							<div class="entry-copyright content-bg p-3">
+							<div class="entry-copyright content-bg p-3 mb-2">
 								<div><i class="fa fa-exclamation-triangle pr-2 "></i> <strong>版权声明：</strong> <span>本文由<a href="<?php the_permalink(); ?>" rel="bookmark" title="本文固定链接 <?php the_permalink(); ?>"> <?php bloginfo( 'name' ); ?> </a> 整理发表，转载请注明出处</span> </div>
 								<div><i class="fa fa-bullseye pr-2 "></i> <strong>转载信息：</strong> <span><a href="<?php the_permalink(); ?>" rel="bookmark" title="本文固定链接 <?php the_permalink(); ?>"> <?php the_title(); ?> | <?php bloginfo( 'name' ); ?></a></span> </div>
 							</div>
 							<?php if ( lerm_options( 'related_posts' ) ) : ?>
-								<section id="related" class="p-2 mt-2 mb-2 border-radus-none">
-									<?php related_posts(); ?>
+								<section id="related" class="mb-2">
+									<?php lerm_related_posts(); ?>
 								</section>
 								<?php
 							endif;
