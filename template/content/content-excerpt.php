778994cc5109e377678ddd4a3141ff8e2eacee54
hanlong@live.cn <hanlong@live.cn>
2020-01-01 17:30:01 +0800

    fix code

--- template/content/content-excerpt.php
+++ template/content/content-excerpt.php
@@ -16,21 +16,17 @@
 		)
 	);
 	?>
-		<div class="content-area d-flex flex-column  justify-content-between">
-				<header class="summary-header">
-					<?php
-							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a><label class="badge badge-danger sticky-label">' . __( 'Sticky', 'lerm' ) . '</label></h2>' );
-					?>
-				</header>
-				<div class="summary-content d-none d-md-block">
-						<?php the_excerpt(); ?>
-				</div>
-				<footer class="text-muted d-flex justify-content-between">
-						<?php
-							echo '<small class="entry-meta">';
-							lerm_entry_meta( 'summary' );
-							echo '</small>';
-						?>
-				</footer>
+	<div class="content-area d-flex flex-column  justify-content-between">
+		<header class="summary-header">
+			<?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a><label class="badge badge-danger sticky-label">' . __( 'Sticky', 'lerm' ) . '</label></h2>' ); ?>
+		</header>
+		<div class="summary-content d-none d-md-block">
+			<?php the_excerpt(); ?>
 		</div>
+		<footer class="text-muted d-flex justify-content-between">
+			<small class="entry-meta">
+				<?php lerm_post_meta( 'summary_bottom' ); ?>
+			</small>
+		</footer>
+	</div>
 </article>
--- template/content/content-single.php
+++ template/content/content-single.php
@@ -10,12 +10,10 @@ $like_count = get_post_meta( $post->ID, 'lerm_post_like', true ) ? get_post_meta
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'entry p-3 mb-2' ); ?> >
 	<header class="entry-header d-flex flex-column justify-content-between mb-md-2 text-center pb-2">
-		<?php
-		the_title( '<h1 class="entry-title test">', '</h1>' );
-		echo '<small class="entry-meta text-muted">';
-		lerm_entry_meta( 'single' );
-		echo '</small>';
-		?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<small class="entry-meta text-muted">;
+			<?php lerm_post_meta( 'single_top' ); ?>
+		</small>
 	</header>
 
 	<div class="entry-content pt-2">
@@ -54,38 +52,16 @@ $like_count = get_post_meta( $post->ID, 'lerm_post_like', true ) ? get_post_meta
 						<?php echo esc_attr( $like_count ); ?>
 					</span>
 				</button>
-				<button class="btn-custom btn entry-comment-btn "><?php echo lerm_post_comments_number(); ?> </button>
+				<button class="btn-custom btn entry-comment-btn">
+					<a href="<?php comments_link(); ?>">
+						<i class="fa fa-comment"></i>
+						<?php printf( _nx( '%s comment', '%s comments', get_comments_number(), 'comments title', 'lerm' ), esc_attr( number_format_i18n( get_comments_number() ) ) ); ?>
+					</a>
+				</button>
 			</div><!-- like -->
 		</div><!-- toolbar -->
-		<div class="d-flex justify-content-between">
-			<div class="donate d-none d-md-block" >
-				<button class="donate-btn btn">
-					<span><i class="fa fa-dollar"></i></span>
-				</button>
-				<div class="donate-qrcode">
-					<img src="<?php echo esc_attr( lerm_options( 'donate-qrcode', 'url' ) ); ?>" alt="donate me" width="100" height="100">
-				</div>
-			</div><!-- donate -->
-			<div id="share" class="d-flex justify-content-end">
-				<ul class="d-flex list-unstyled mb-0">
-					<li title="分享到QQ空间" class="mr-2">
-						<a class="qzone border rounded p-1 text-primary">
-							<i class="fa fa-qq"></i>
-						</a>
-					</li>
-					<li title="分享到新浪微博" class="mr-2">
-						<a class="tsina border rounded p-1 text-danger">
-							<i class="fa fa-weibo"></i>
-						</a>
-					</li>
-					<li title="分享到微信" class="share-code position-relative mr-2">
-						<a class="wechat border rounded p-1 text-success">
-							<i class="fa fa-wechat"></i>
-						</a>
-						<div id="share-qrcode" class="towdimcodelayer"></div>
-					</li>
-				</ul>
-			</div>
+		<div class="d-flex justify-content-center justify-content-md-end">
+			<?php get_template_part( 'template/post/share' ); ?>
 		</div>
 	</footer>
 </article>
