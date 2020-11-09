69b534d72bc5d20132f7ce69835df3fa79fa5eb4
hanlong@live.cn <hanlong@live.cn>
2020-11-09 16:41:40 +0800

    add post header

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -9,18 +9,9 @@ global $post;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'col' ); ?> >
 	<div class="content-area">
-		<header class="entry-header d-flex flex-column justify-content-between mb-md-2" style="background-image: url(<?php echo sunset_get_attachment(); ?>);">
-			<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a><label class="sticky-label badge badge-danger">' . __( 'Sticky', 'lerm' ) . '</label></h2>' );
-			}
-			?>
-			<small class="entry-meta text-muted">
-				<?php lerm_post_meta( 'summary_bottom' ); ?>
-			</small>
-		</header>
+
+		<?php get_template_part('template-parts/header/entry-header') ?>
+
 
 		<div class="entry-content pt-2">
 			<?php the_excerpt(); ?>
@@ -33,4 +24,3 @@ global $post;
 				</div><!-- .entry-tags -->
 			</small>
 		</footer>
-</article>
