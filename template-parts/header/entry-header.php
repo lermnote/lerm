6587148ee681f2be0120544659201845f2c0c936
lermnote <hanlong@live.cn>
2020-12-17 20:56:56 +0800

    修改文章页标题

--- template-parts/header/entry-header.php
+++ template-parts/header/entry-header.php
@@ -8,11 +8,9 @@
 
 <header class="entry-header d-flex flex-column text-center mb-md-2">
 	<?php
-	if ( is_singular() ) {
-		the_title( '<h1 class="entry-title">', '</h1>' );
+	the_title( '<h1 class="entry-title">', '</h1>' );
+	if ( is_singular( 'post' ) ) {
 		lerm_post_meta( 'single_top' );
-	} else {
-
 	}
 	?>
 </header>
